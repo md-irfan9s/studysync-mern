222a107a9226a439b4296119f39bf8f70ae74b5c
md-irfan9s <mdirfancs07@gmail.com>
2025-10-16 20:19:22 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(
 	cors({
 		origin: [
 			"http://localhost:3000",                 // dev
-			"https://studysync-mern.vercel.app/"
+			"https://studysync-mern.vercel.app"
 		],
 		
 		credentials: true,
